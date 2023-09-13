c9cd398bb95973dabc5e3990adaaedd24a8b1603
Marko Glavic <glavim@gmail.com>
2023-09-13 10:58:24 +0100

    Added allOlder exercise

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,1 +1,19 @@
 import {Friend, Colleague } from './myTypes'
+
+import { friends, colleagues } from "./01-basics";
+
+function older(f: Friend) : string {
+     f.age += 1
+     return `${f.name} is now ${f.age}` 
+}
+
+function allOlder(f: typeof friends) : string[] {
+
+  return  f.map((friend) =>  `${friend.name} is now ${friend.age + 1}`)
+
+
+   
+}
+//console.log(older(friends[0]))
+console.log(allOlder(friends))
+
